675a008fba39d3fdb4739d4f0b92fcc58307fedf
Adam Weissman <adam@comedytragedyepic.com>
2019-09-19 11:17:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  return programmer_hash[:dennis_richie][:languages]
+  return programmer_hash[:dennis_richie][:languages][0]
     
 end
 
